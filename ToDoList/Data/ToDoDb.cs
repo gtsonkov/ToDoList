9c40942ac6e9ad3ad2eb69c8ca897b253d55d2ca
gtsonkov <gtsonkov@abv.bg>
2019-04-13 22:30:38 +0200

    Initial ToDoList

---
+++ ToDoList/Data/ToDoDb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Data 
+{
+    using Microsoft.EntityFrameworkCore;
+    using ToDoList.Models;
+    public class ToDoDb:DbContext
+    {
+        public DbSet<Task> Task { get; set; }
+
+        private const string ConnectionString = @"Server=.\TSSQL;Database=ToDoDb;Integrated Security=true;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
